9d212de8f49f89525476d8c53cf315ba9bb50f69
adevone <terehovks3@gmail.com>
2019-09-15 21:09:42 +0500

    Fix docs

--- summer/src/commonMain/kotlin/summer/SummerPresenter.kt
+++ summer/src/commonMain/kotlin/summer/SummerPresenter.kt
@@ -51,11 +51,8 @@ abstract class SummerPresenter<
         this._viewMethods = viewMethods
         this._router = router
 
-        // In after create because some properties call presenter in initView and
-        // presenter must be initialized at this moment
-        //
-        // Некоторые проперти при изменении значений обращаются к presenter'у.
-        // Поэтому presenter должен быть инициализирован в момент их установки
+        // Placed there because presenter methods may be called in initView.
+        // Presenter must be initialized at that moment
         store.restore()
     }
 
