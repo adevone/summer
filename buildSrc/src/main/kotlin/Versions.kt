7ad3ccd26df4074cad0f6c051f09031b28fe6ebe
adevone <terehovks3@gmail.com>
2019-10-15 12:38:45 +0500

    Split versions to library versions and example versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,10 +1,13 @@
 const val summerVersion = "0.6.18"
 
+// library dependencies
 const val kotlinVersion = "1.3.50"
+const val coroutinesVersion = "1.3.2"
+
+// example dependencies
 const val multiplatformSettingVersion = "0.3.3"
 const val atomicfuVersion = "0.12.11"
 const val ktorVersion = "1.2.5"
-const val coroutinesVersion = "1.3.2"
 const val kodeinVersion = "6.4.0"
 const val serializationVersion = "0.13.0"
 
