c382809aa46c333f1645ec7717603bbc788265d6
adevone <terehovks3@gmail.com>
2019-10-19 15:57:19 +0500

    Migrate SummerComponent.kt to SummerPresenter and SummerPresenterWithRouter as separate classes

--- summer-androidx/src/main/java/summer/android/SummerComponent.kt
+++ summer-androidx/src/main/java/summer/android/SummerComponent.kt
@@ -4,14 +4,13 @@ import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import summer.SummerPresenter
+import summer.SummerPresenterWithRouter
 
 abstract class SummerComponent<
         TViewState : Any,
         TViewMethods : Any,
-        TRouter : Any,
-        TPresenter : SummerPresenter<TViewState, TViewMethods, TRouter>> {
+        TPresenter : SummerPresenter<TViewState, TViewMethods>> {
 
-    protected abstract val router: TRouter
     protected abstract val viewMethods: TViewMethods
 
     protected abstract val presenter: TPresenter
@@ -37,15 +36,19 @@ abstract class SummerComponent<
     fun onViewCreated(parentView: ViewGroup?, context: Context, isFirstViewCreation: Boolean) {
         _view = createView(parentView, context)
         initView()
-        presenter.viewCreated(viewState, viewMethods, router)
+        initPresenterView()
         if (isFirstViewCreation) {
             presenter.entered()
         }
     }
 
+    internal open fun initPresenterView() {
+        presenter.viewCreated(viewState, viewMethods)
+    }
+
     protected abstract fun initView()
 
-    fun onDestroyView() {
+    open fun onDestroyView() {
         presenter.viewDestroyed()
         _view = null
     }
@@ -60,5 +63,28 @@ abstract class SummerComponent<
 
     fun onExit() {
         presenter.exited()
+    }
+}
+
+abstract class SummerComponentWithRouter<
+        TViewState : Any,
+        TViewMethods : Any,
+        TRouter : Any,
+        TPresenter : SummerPresenterWithRouter<TViewState, TViewMethods, TRouter>>
+    : SummerComponent<
+        TViewState,
+        TViewMethods,
+        TPresenter>() {
+
+    protected abstract val router: TRouter
+
+    override fun initPresenterView() {
+        super.initPresenterView()
+        presenter.routerCreated(router)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        presenter.viewDestroyed()
     }
 }
