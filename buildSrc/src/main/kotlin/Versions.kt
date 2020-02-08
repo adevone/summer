6270158f18344ebdaf67dde8f0b0772ec3b5979d
adevone <terehovks3@gmail.com>
2020-02-08 21:35:52 +0500

    Work in progress

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,14 +1,14 @@
-const val summerVersion = "0.8.12"
+const val summerVersion = "0.8.13"
 
 // library dependencies
 const val kotlinVersion = "1.3.61"
-const val coroutinesVersion = "1.3.2-1.3.60"
+const val coroutinesVersion = "1.3.3"
 
 // example dependencies
-const val multiplatformSettingVersion = "0.4.1"
+const val multiplatformSettingVersion = "0.5"
 const val atomicfuVersion = "0.14.1"
-const val ktorVersion = "1.2.6"
-const val kodeinVersion = "6.5.0"
+const val ktorVersion = "1.3.1"
+const val kodeinVersion = "6.5.1"
 const val serializationVersion = "0.14.0"
 
 const val targetVersion = 29
