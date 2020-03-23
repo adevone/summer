ef7f720b215c5916ee1af1965fcfc0faee346bf0
adevone <terehovks3@gmail.com>
2020-03-24 02:04:33 +0500

    Migrate summer-androix to 1.0.0-beta1

--- summer-androidx/src/main/java/summer/android/bundle/BundleProvider.kt
+++ summer-androidx/src/main/java/summer/android/bundle/BundleProvider.kt
@@ -1,10 +1,10 @@
 package summer.android.bundle
 
 import android.os.Bundle
-import summer.state.StateDelegate
+import summer.state.SummerStateDelegate
 
 interface BundleProvider {
     val bundle: Bundle
 }
 
-typealias BundleStateDelegateProvider<T> = StateDelegate.Provider<T, BundleProvider>
+typealias BundleStateDelegateProvider<T> = SummerStateDelegate.Provider<T, BundleProvider>
