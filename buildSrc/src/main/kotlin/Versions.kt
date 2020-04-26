6bc8b87b2e4b93313946b446df7c907ad30bdd64
kirill.terekhov <kirill.terekhov@alibaba-inc.com>
2020-04-26 21:37:25 +0300

    Upgrade dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,15 +2,15 @@ const val summerVersion = "1.0.0-beta1"
 const val summerGroup = "com.github.adevone.summer"
 
 // library dependencies
-const val kotlinVersion = "1.3.70"
+const val kotlinVersion = "1.3.72"
 
 // example dependencies
 const val multiplatformSettingVersion = "0.5.1"
-const val kodeinVersion = "6.5.3"
+const val kodeinVersion = "6.5.5"
 const val serializationVersion = "0.20.0"
 const val ktorVersion = "1.3.2"
-const val coroutinesVersion = "1.3.4"
-const val atomicfuVersion = "0.14.2"
+const val coroutinesVersion = "1.3.5"
+const val atomicfuVersion = "0.14.3"
 
 const val targetVersion = 29
 const val minVersion = 21
