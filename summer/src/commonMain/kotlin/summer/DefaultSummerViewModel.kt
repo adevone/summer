897c873a37c7497ed60af014e6a76a78a7e8a5d3
kirill.terekhov <kirill.terekhov@alibaba-inc.com>
2021-01-01 14:56:43 +0300

    Move default states and strategies connecting logic to DefaultSummerViewModel.kt

---
+++ summer/src/commonMain/kotlin/summer/DefaultSummerViewModel.kt
@@ -0,0 +1,48 @@
+package summer
+
+import summer.events.DoExactlyOnceStrategy
+import summer.events.DoOnlyWhenAttachedStrategy
+import summer.events.EventFactory
+import summer.events.SummerEventStrategy
+import summer.state.*
+
+/**
+ * View model with support of default
+ * - [SummerEventStrategy]: [DoOnlyWhenAttachedStrategy], [DoExactlyOnceStrategy]
+ * - [SummerStateStrategy]: [InMemoryStateStrategy]
+ *
+ * Implemented in [DefaultSummerViewModelImpl]
+ *
+ * @param [TView] see [LifecycleViewModel]
+ */
+interface DefaultSummerViewModel<TView> :
+    EnterLifecycleViewModel<TView>,
+    StateFactory<TView, InMemoryStoreProvider>,
+    EventFactory<TView, GetViewProvider<TView>>,
+    InMemoryStateStrategy.Factory<TView>,
+    InMemoryStoreProvider,
+    DoOnlyWhenAttachedStrategy.Factory<TView>,
+    DoExactlyOnceStrategy.Factory<TView>
+
+/**
+ * Implementation of [DefaultSummerViewModel].
+ * Can be used as a base class or an interface delegate.
+ *
+ * Example of a delegation:
+ * class MyViewModel<TView> :
+ *     <parent class that prohibits inheritance from DefaultSummerViewModelImpl>,
+ *     DefaultSummerViewModel<TView> by DefaultSummerViewModelImpl()
+ * @param [TView] see [LifecycleViewModel]
+ */
+open class DefaultSummerViewModelImpl<TView> :
+    DefaultSummerViewModel<TView>,
+    RestoreViewModel<TView, InMemoryStoreProvider, GetViewProvider<TView>>() {
+
+    /**
+     * Used for state storing by default. Can be overridden.
+     */
+    override val inMemoryStore = InMemoryStore()
+
+    override fun getEventsOwner(): GetViewProvider<TView> = this
+    override fun getStateOwner(): InMemoryStoreProvider = this
+}
--- summer/src/commonMain/kotlin/summer/SummerViewModel.kt
+++ summer/src/commonMain/kotlin/summer/SummerViewModel.kt
@@ -1,30 +1,14 @@
 package summer
 
-import summer.events.DoExactlyOnceStrategy
-import summer.events.DoOnlyWhenAttachedStrategy
 import summer.events.SummerEvent
-import summer.state.InMemoryStateStrategy
-import summer.state.InMemoryStore
-import summer.state.InMemoryStoreProvider
 import summer.state.SummerStateDelegate
 
 /**
  * Base viewModel. Helps with view state restoring (see [SummerStateDelegate])
  * and executing of events (see [SummerEvent]).
+ *
+ * @param [TView] see [LifecycleViewModel]
  */
 abstract class SummerViewModel<TView> :
-    RestoreViewModel<TView, InMemoryStoreProvider, GetViewProvider<TView>>(),
-    ViewProxyProvider<TView>,
-    DoOnlyWhenAttachedStrategy.Factory<TView>,
-    DoExactlyOnceStrategy.Factory<TView>,
-    InMemoryStateStrategy.Factory<TView>,
-    InMemoryStoreProvider {
-
-    /**
-     * Used for state storing by default. Can be overridden.
-     */
-    override val inMemoryStore = InMemoryStore()
-
-    override fun getEventsOwner(): GetViewProvider<TView> = this
-    override fun getStateOwner(): InMemoryStoreProvider = this
-}
+    DefaultSummerViewModelImpl<TView>(),
+    ViewProxyProvider<TView>
