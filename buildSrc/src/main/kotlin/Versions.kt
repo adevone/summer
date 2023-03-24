c0136b93032e10f930de2011f44209adbe971c36
adevone <terehovks3@gmail.com>
2023-03-24 14:24:45 +0300

    Downgrade dependencies, Publish 1.0.0-beta13

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,10 +1,10 @@
-const val summerVersion = "1.0.0-beta12"
+const val summerVersion = "1.0.0-beta13"
 const val summerGroup = "com.github.adevone.summer"
 
 // library dependencies
 const val kotlinVersion = "1.7.10"
-const val lifecycleVersion = "2.4.1"
-const val appCompatVersion = "1.4.1"
+const val lifecycleVersion = "2.3.1"
+const val appCompatVersion = "1.3.1"
 const val serializationVersion = "1.3.2"
 const val targetVersion = 33
 
